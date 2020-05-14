750544b12cc9cbb64b0c5d087e4e216cf4ecebb0
Adam Green <adgreenie@gmail.com>
2020-05-13 21:27:50 -0400

    added loading screen for game and leaderboard

--- trivia/src/components/Leaderboard.js
+++ trivia/src/components/Leaderboard.js
@@ -1,57 +1,53 @@
-import React from 'react'
+import React from "react"
+import PulseLoader from "react-spinners/PulseLoader"
 
 function Leaderboard(props) {
-    //console.log('Leaderboard - props.gameView',props.gameView)
-    
-    let scoreList = []
-    
-    if (props.highScores) {
+  //console.log('Leaderboard - props.gameView',props.gameView)
 
-        let fontSize = props.gameView ? 24 : 42
+  let scoreList = []
 
-        scoreList = props.highScores.map((highScore, i) => {
-            let dots = ' . . '   
-            for (let j = highScore.name.length; j < 12; j += 2) {
-                dots += '. '
-            }
-            if (i > 0) {
-                let mult = (i < 3 ? 2 : 1)
-                props.gameView ? fontSize -= 1 * mult : fontSize -= 3 * mult
-            }
-            console.log(props.scoreIndex)
-            let color = (props.scoreIndex === i ? '#69c07e' : 'black')
-            return (
-                <li style={{fontSize: `${fontSize}px`, color: color}} key={i}>
-                    {i + 1}) {highScore.name} {dots} {highScore.score}
-                </li>
-            ) 
-        })
-    }
+  if (props.highScores) {
+    let fontSize = props.gameView ? 24 : 42
 
-    if (props.gameView) {
-        return (
-            <div className="leaderboard" id="in-game-leaderboard">
-                <h2>Leaderboard</h2>
-                <hr />
-                <ul>
-                    {scoreList}
-                </ul>
-            </div>
-        )
-    } else {
-        return (
-            <div className="leaderboard" id="full-leaderboard">
-                <div>
-                    <h1>Wall of Glory</h1>
-                    <hr />
-                </div>
-                <ul>
-                    {scoreList}
-                </ul>
-                <img src="https://res.cloudinary.com/dnj7porin/image/upload/v1585808926/crowdBG_ex9dzj.png" />
-            </div>
-        )
-    }
+    scoreList = props.highScores.map((highScore, i) => {
+      let dots = " . . "
+      for (let j = highScore.name.length; j < 12; j += 2) {
+        dots += ". "
+      }
+      if (i > 0) {
+        let mult = i < 3 ? 2 : 1
+        props.gameView ? (fontSize -= 1 * mult) : (fontSize -= 3 * mult)
+      }
+      console.log(props.scoreIndex)
+      let color = props.scoreIndex === i ? "#69c07e" : "black"
+      return (
+        <li style={{ fontSize: `${fontSize}px`, color: color }} key={i}>
+          {i + 1}) {highScore.name} {dots} {highScore.score}
+        </li>
+      )
+    })
+  }
+
+  if (props.gameView) {
+    return (
+      <div className="leaderboard" id="in-game-leaderboard">
+        <h2>Leaderboard</h2>
+        <hr />
+        <ul>{scoreList}</ul>
+      </div>
+    )
+  } else {
+    return (
+      <div className="leaderboard" id="full-leaderboard">
+        <div>
+          <h1>Wall of Glory</h1>
+          <hr />
+        </div>
+        {scoreList ? <ul>{scoreList}</ul> : <PulseLoader color={"#69c07e"} />}
+        <img src="https://res.cloudinary.com/dnj7porin/image/upload/v1585808926/crowdBG_ex9dzj.png" />
+      </div>
+    )
+  }
 }
 
 export default Leaderboard
--- trivia/src/components/game/Gameboard.js
+++ trivia/src/components/game/Gameboard.js
@@ -1,110 +1,120 @@
-import React, { useEffect, useState, createContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from 'reactstrap'
-import Question from './Question'
-import HighScore from './HighScore'
-import Leaderboard from '../Leaderboard'
-import './game.scss'
+import React, { useEffect, useState, createContext } from "react"
+import { Link } from "react-router-dom"
+import { Button } from "reactstrap"
+import Question from "./Question"
+import HighScore from "./HighScore"
+import Leaderboard from "../Leaderboard"
+import PulseLoader from "react-spinners/PulseLoader"
+import "./game.scss"
 
 export const DataContext = createContext()
 
 function Gameboard(props) {
-    const [questionArr, setQuestionArr] = useState(false)
-    const [qNum, setQNum] = useState(1)
-    const [isAnswered, setIsAnswered] = useState(false)
-    const [isGameOver, setIsGameOver] = useState(false)
-    const [nextVis, setNextVis] = useState('hidden')
-    const [nextOpacity, setNextOpacity] = useState(0)
+  const [questionArr, setQuestionArr] = useState(false)
+  const [qNum, setQNum] = useState(1)
+  const [isAnswered, setIsAnswered] = useState(false)
+  const [isGameOver, setIsGameOver] = useState(false)
+  const [nextVis, setNextVis] = useState("hidden")
+  const [nextOpacity, setNextOpacity] = useState(0)
 
-    const calcScore = isCorrect => {
-        setIsAnswered(true)
+  const calcScore = (isCorrect) => {
+    setIsAnswered(true)
 
-        if (isCorrect) {
-            const diffs = {
-                'easy' : 1,
-                'medium' : 2,
-                'hard' : 3
-            }
-            props.dispatch({type: "ADD", value: diffs[questionArr[qNum - 1].difficulty]})
-        } else {
-            props.dispatch({type: "SUB"})
-        }
-
-        setNextVis('visible')
-        setNextOpacity(1)
+    if (isCorrect) {
+      const diffs = {
+        easy: 1,
+        medium: 2,
+        hard: 3,
+      }
+      props.dispatch({
+        type: "ADD",
+        value: diffs[questionArr[qNum - 1].difficulty],
+      })
+    } else {
+      props.dispatch({ type: "SUB" })
     }
 
-    const nextQuestion = () => {
-        setNextVis('hidden')
-        setNextOpacity(0)
+    setNextVis("visible")
+    setNextOpacity(1)
+  }
 
-        qNum < 10 ? setQNum(qNum + 1) : gameOver()
+  const nextQuestion = () => {
+    setNextVis("hidden")
+    setNextOpacity(0)
 
-        setIsAnswered(false)
+    qNum < 10 ? setQNum(qNum + 1) : gameOver()
+
+    setIsAnswered(false)
+  }
+
+  const gameOver = () => {
+    setIsGameOver(true)
+    props.checkForHighScore()
+  }
+
+  useEffect(() => {
+    const makeApiCall = async () => {
+      const catNum = props.catIndex ? parseInt(props.catIndex) + 9 : ""
+      const url = `https://opentdb.com/api.php?amount=10&category=${catNum}&difficulty=${props.difficulty}`
+      console.log("Gameboard - makeApiCall - url", url)
+      const res = await fetch(url)
+      const json = await res.json()
+      console.log("Gameboard - makeApiCall - json.results", json.results)
+      setQuestionArr(json.results)
     }
+    makeApiCall()
+    props.dispatch({ type: "RESET" })
+  }, [])
 
-    const gameOver = () => {
-        setIsGameOver(true)
-        props.checkForHighScore()
-    }
-    
-    useEffect(() => {
-        const makeApiCall = async () => {
-            const catNum = props.catIndex ? parseInt(props.catIndex) + 9 : ''
-            const url = `https://opentdb.com/api.php?amount=10&category=${catNum}&difficulty=${props.difficulty}`
-            console.log('Gameboard - makeApiCall - url', url)
-            const res = await fetch(url)
-            const json = await res.json()
-            console.log('Gameboard - makeApiCall - json.results', json.results)
-            setQuestionArr(json.results)
-        }
-        makeApiCall()
-        props.dispatch({type: "RESET"})
-    }, [])
-    
-    if (!questionArr) {
-        return <></>
-    }
-
-    const qDisplay = (
-        <div className="gameboard">
-            <h2>Question {qNum}</h2>
-            <DataContext.Provider value={ {calcScore, isAnswered} }>
-                <Question
-                    qData={questionArr[qNum - 1]}
-                    catIndex={props.catIndex}
-                    difficulty={props.difficulty}
-                    categoryArr={props.categoryArr}
-                />
-            </DataContext.Provider>
-            <span id="score">Score: {props.score}</span>
-            <button style={{visibility: nextVis, opacity: nextOpacity}}
-                onClick={nextQuestion}>Next</button>
-            <Leaderboard gameView={true} highScores={props.highScores} />
-        </div>
+  if (!questionArr) {
+    return (
+      <div className="loading">
+        <PulseLoader color={"#69c07e"} />
+      </div>
     )
+  }
 
-    const endDisplay = (
-        <div className="game-over">
-            <h2>Final score:</h2>
-            <h1>{props.score}</h1>
-            <h3>Thanks for playing!</h3>
-            <Link to="/leaderboard">
-                Check the Leaderboard
-            </Link>
-            <Button className="button" onClick={() => window.location.reload()}>
-                Start A New Game
-            </Button>
-        </div>
-    )
-    
-    return ( 
-        !isGameOver ?
-            qDisplay :
-            props.checkForHighScore() ?
-                <HighScore score={props.score} submitScore={props.submitScore} /> :
-                endDisplay
-    )
+  const qDisplay = (
+    <div className="gameboard">
+      <h2>Question {qNum}</h2>
+      <DataContext.Provider value={{ calcScore, isAnswered }}>
+        <Question
+          qData={questionArr[qNum - 1]}
+          catIndex={props.catIndex}
+          difficulty={props.difficulty}
+          categoryArr={props.categoryArr}
+        />
+      </DataContext.Provider>
+      <span id="score">Score: {props.score}</span>
+      <button
+        style={{ visibility: nextVis, opacity: nextOpacity }}
+        onClick={nextQuestion}
+      >
+        Next
+      </button>
+      <Leaderboard gameView={true} highScores={props.highScores} />
+    </div>
+  )
+
+  const endDisplay = (
+    <div className="game-over">
+      <h2>Final score:</h2>
+      <h1>{props.score}</h1>
+      <h3>Thanks for playing!</h3>
+      <Link to="/leaderboard">Check the Leaderboard</Link>
+      <Button className="button" onClick={() => window.location.reload()}>
+        Start A New Game
+      </Button>
+    </div>
+  )
+
+  return !isGameOver ? (
+    qDisplay
+  ) : props.checkForHighScore() ? (
+    <HighScore score={props.score} submitScore={props.submitScore} />
+  ) : (
+    endDisplay
+  )
 }
 
 export default Gameboard
